3f76d867ee9fffdd15e74d41dc7bc8fc2bda378d
EvgenyShpagin <shpagin.from.usa@gmail.com>
2024-08-21 18:46:35 +0700

    fix: переопределение hashCode и equals

--- app/src/main/java/com/tusur/teacherhelper/presentation/model/UiText.kt
+++ app/src/main/java/com/tusur/teacherhelper/presentation/model/UiText.kt
@@ -5,8 +5,36 @@ import androidx.annotation.StringRes
 
 sealed class UiText {
 
-    data class Dynamic(val value: String) : UiText()
-    class Resource(@StringRes val resId: Int, vararg val args: Any) : UiText()
+    data class Dynamic(val value: String) : UiText() {
+        override fun hashCode(): Int {
+            return 31 * value.hashCode()
+        }
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as Dynamic
+
+            return value == other.value
+        }
+    }
+
+    class Resource(@StringRes val resId: Int, vararg val args: Any) : UiText() {
+        override fun hashCode(): Int {
+            return 31 * resId + args.contentHashCode()
+        }
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as Resource
+
+            if (resId != other.resId) return false
+            return args.contentEquals(other.args)
+        }
+    }
 
     fun toString(context: Context): String {
         return when (this) {
