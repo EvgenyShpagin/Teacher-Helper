0e028deacda853cdced090864d1a100517f927f2
EvgenyShpagin <shpagin.from.usa@gmail.com>
2024-11-18 17:28:54 +0700

    fix: repair replace check use case

--- app/src/main/java/com/tusur/teacherhelper/domain/usecase/CanTopicTypeBeReplacedUseCase.kt
+++ app/src/main/java/com/tusur/teacherhelper/domain/usecase/CanTopicTypeBeReplacedUseCase.kt
@@ -7,8 +7,10 @@ class CanTopicTypeBeReplacedUseCase() {
         if (fromType == toType) return true
         val assessmentRefused = fromType.isAssessmentAcceptable && !toType.isAssessmentAcceptable
         val attendanceRefused = fromType.isAttendanceAcceptable && !toType.isAttendanceAcceptable
+        val attendanceDecreased = fromType.isAttendanceAcceptable && toType.isAttendanceAcceptable
+                && !fromType.isAttendanceForOneClassOnly && toType.isAttendanceForOneClassOnly
         val gradeRefused = fromType.isGradeAcceptable && !toType.isGradeAcceptable
         val progressRefused = fromType.isProgressAcceptable && !toType.isProgressAcceptable
-        return !(assessmentRefused || attendanceRefused || gradeRefused || progressRefused)
+        return !(assessmentRefused || attendanceRefused || attendanceDecreased || gradeRefused || progressRefused)
     }
 }
