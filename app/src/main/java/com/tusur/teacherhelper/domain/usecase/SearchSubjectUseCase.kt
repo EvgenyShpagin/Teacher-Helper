1eef150707c2d0a37ec5c02c05a0ddaa17e0e3c8
EvgenyShpagin <shpagin.from.usa@gmail.com>
2025-07-07 14:27:25 +0700

    fix(domain/SearchSubjectUseCase): filter out global topics subject

--- app/src/main/java/com/tusur/teacherhelper/domain/usecase/SearchSubjectUseCase.kt
+++ app/src/main/java/com/tusur/teacherhelper/domain/usecase/SearchSubjectUseCase.kt
@@ -2,10 +2,12 @@ package com.tusur.teacherhelper.domain.usecase
 
 import com.tusur.teacherhelper.domain.model.Subject
 import com.tusur.teacherhelper.domain.repository.SubjectRepository
+import com.tusur.teacherhelper.domain.util.GLOBAL_TOPICS_SUBJECT_ID
 import jakarta.inject.Inject
 
 class SearchSubjectUseCase @Inject constructor(private val subjectRepository: SubjectRepository) {
     suspend operator fun invoke(query: String): List<Subject> {
         return subjectRepository.search(query)
+            .filterNot { it.id == GLOBAL_TOPICS_SUBJECT_ID }
     }
 }
